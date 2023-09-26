653199ad35bca72da0b547282f3c7f6925419622
Ibneashraf <ibneashrafmadani@gmail.com>
2023-09-26 13:42:08 +0500

    git commit -m "initial commit"

---
+++ app.js
@@ -0,0 +1,58 @@
+// // var myName = "My name is ibne Asraf"
+
+// console.log("Hello... I can run JS through my web browser console" );
+// console.log(myName)
+// // 
+// var num witkout coting
+// // var num =10
+// // alert(num)
+// // console.log(num+5)
+// agr num ko cotation me rakhen to ab 5 string m convert hojae ga
+// // var num ="10"
+// // alert(num)
+// // console.log(num+5)
+// // 
+// agr string me likhen to (+) me value  plus nhi hogi
+// //  var num1 = "5";
+// //  var num2 = 3;
+// //  alert(num1+num2)
+//  agr string me likhen to (+) k ilawa (-.*./) me ok work krega
+//  var num1 = "5";
+//  var num2 = 3;
+//  alert(num1-num2)
+// (/):
+//  var num1 = "10";
+//  var num2 = 2;
+//  alert(num1/num2)
+// (*):
+//  var num1 = "5";
+//  var num2 = 3;
+//  alert(num1*num2)
+
+// is sign(%) ko Js me modulas signe krhtr hen
+//  var num1 = 20;
+//  var num2 = 5;
+//  alert(num1%num2)
+ //var name illegal batter he
+//  var num="Ibne ashraf"
+//  var str= 10
+
+ // cal++ post increment
+ var cal=20
+ var cal2= ++cal;
+ console.log(cal2)
+
+ var num=10
+ var num1=20
+           11    + 20
+ var num2= + +num + num1
+ console.log(num2)
+ console.log(num) // ab 11 ae ga qk niche  update ho chuka he
+
+// ++call pre increment
+// call-- post decrement
+// --call post decrement
+
+//  alert("Please enter a valid passward")
+//  alert("welcome to JS land \n Happy coding!")
+//  alert("welcome to JS code")
